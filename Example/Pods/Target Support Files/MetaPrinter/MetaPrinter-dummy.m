0b318467e8aee5add2d26a6917ede73d35df05d7
唐梓皓 <tangzihao@tangzihaodeMBP.lan>
2022-03-19 14:04:33 +0800

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/MetaPrinter/MetaPrinter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MetaPrinter : NSObject
+@end
+@implementation PodsDummy_MetaPrinter
+@end
---
+++ Example/Pods/Target Support Files/Pods-MetaPrinter_Tests/Pods-MetaPrinter_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MetaPrinter_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MetaPrinter_Tests
+@end
